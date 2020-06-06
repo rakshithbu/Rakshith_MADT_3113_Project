a20957c0b94e33cecdb3cff87197aaf36f09fd90
rakshith <rakshithbu92@gmail.com>
2020-06-06 02:10:22 -0400

    implemented registration using index.db

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,5 @@
-/*
- Template Name: vacayhome
- File Name: custom.js
- Author Name: ThemeVault
- Author URI: http://www.themevault.net/
- License URI: http://www.themevault.net/license/
- */
+
+let db;
 
 $(document).ready(function () {
     $(window).scroll(function () {
@@ -19,7 +14,160 @@ $(document).ready(function () {
         return false;
     });
 
-        $('.date').datepicker();
+    var options={
+        format: 'mm/dd/yyyy',
+        todayHighlight: true,
+        autoclose: true,
+    };
+    $('#date').datepicker(options);
+
 })
 
 
+
+    let request = indexedDB.open('RVVHome', 1);
+
+    request.onerror = function () {
+        console.log('Database failed to open');
+    };
+
+// onsuccess handler signifies that the database opened successfully
+    request.onsuccess = function (event) {
+        db = event.target.result;
+    };
+
+    request.onupgradeneeded = function(e) {
+        // Grab a reference to the opened database
+        db = e.target.result;
+        let objectStore = db.createObjectStore('registration', { keyPath: 'id', autoIncrement:true });
+        objectStore.createIndex('firstName', 'firstName', { unique: false });
+        objectStore.createIndex('lastName', 'lastName', { unique: false });
+        objectStore.createIndex('emailId', 'emailId', { unique: true });
+        objectStore.createIndex('sex', 'sex', { unique: false });
+        objectStore.createIndex('dob', 'dob', { unique: false });
+        objectStore.createIndex('password', 'password', { unique: false })
+
+        console.log('Database setup complete');
+    }
+
+
+
+
+function registerUser() {
+    if(validateRegistration()){
+        var db = request.result;
+        let transaction = db.transaction(['registration'], 'readwrite');
+        let objectStore = transaction.objectStore('registration');
+        if ('getAll' in objectStore) {
+            // IDBObjectStore.getAll() will return the full set of items in our store.
+            objectStore.getAll().onsuccess = function(event) {
+                let arrayOfResult = event.target.result;
+                var flag = false;
+                arrayOfResult.forEach(function(element, index) {
+                    if(element.emailId === $("#emailId").val()){
+                        flag = true;
+                    }
+                });
+
+                if(flag){
+                    $("#emailError").show();
+                    $("#emailError").text("This email ID already exists");
+                    setTimeout(function(){
+                        $("#emailError").hide();
+                        $("#emailError").text("");
+                    }, 6000);
+                }else{
+                    objectStore.add({firstName:$("#firstName").val(),lastName:$("#lastName").val(),emailId:$("#emailId").val(),sex:$("input[name='gender']:checked").val(),dob:$("date").val(),password:$("password").val() });
+                }
+            };
+        }
+    }
+
+}
+
+function validateRegistration(){
+
+  if($("#firstName").val() === ""){
+      $("#firstNameError").show();
+      $("#firstNameError").text("Please enter your first name");
+      setTimeout(function(){
+          $("#firstNameError").hide();
+          $("#firstNameError").text("");
+      }, 6000);
+      return false;
+  }
+    if($("#lastName").val() === ""){
+        $("#lastNameError").show();
+        $("#lastNameError").text("Please enter your last name");
+        setTimeout(function(){
+            $("#lastNameError").hide();
+            $("#lastNameErrorsq").text("");
+        }, 6000);
+        return false;
+    }
+    if($("#emailId").val() === ""){
+        $("#emailError").show();
+        $("#emailError").text("Please enter your email id");
+        setTimeout(function(){
+            $("#emailError").hide();
+            $("#emailError").text("");
+        }, 6000);
+        return false;
+    }
+    if($("#gender").val() === ""){
+        $("#genderError").show();
+        $("#genderError").text("Please select your gender");
+        setTimeout(function(){
+            $("#genderError").hide();
+            $("#genderError").text("");
+        }, 6000);
+        return false;
+    }
+    if($("input[name='gender']:checked").val() === "undefined"){
+        $("#dateError").show();
+        $("#dateError").text("Please enter your date of birth");
+        setTimeout(function(){
+            $("#dateError").hide();
+            $("#dateError").text("");
+        }, 6000);
+        return false;
+    }
+    if($("#password").val() === ""){
+        $("#passwordError").show();
+        $("#passwordError").text("Please enter your password");
+        setTimeout(function(){
+            $("#passwordError").hide();
+            $("#passwordError").text("");
+        }, 6000);
+        return false;
+    }
+    return true;
+}
+
+function login(){
+    var db = request.result;
+    let transaction = db.transaction(['registration'], 'readwrite');
+    let objectStore = transaction.objectStore('registration');
+    if ('getAll' in objectStore) {
+        // IDBObjectStore.getAll() will return the full set of items in our store.
+        objectStore.getAll().onsuccess = function(event) {
+            let arrayOfResult = event.target.result;
+            var flag = false;
+            var name ='';
+            arrayOfResult.forEach(function(element, index) {
+                if((element.emailId === $("#emailId").val()) &&
+                    (element.password === $("#password").val())){
+                    flag = true;
+                    name = element.firstName+" "+ element.lastName;
+                }
+            });
+
+            if(flag){
+                alert("Welcome "+element.)
+            }else{
+                objectStore.add({firstName:$("#firstName").val(),lastName:$("#lastName").val(),emailId:$("#emailId").val(),sex:$("input[name='gender']:checked").val(),dob:$("date").val(),password:$("password").val() });
+            }
+        };
+    }
+
+}
